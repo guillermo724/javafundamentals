2f499f0f433fba3fcff9a12ce0e4d5d379b6a25c
guillermo724 <gmiguel08@colegiovirgendeeuropa.edu.es>
2026-09-10 10:29:19 +0200

    firts commit

---
+++ src/fundamentals/VariableTest.java
@@ -0,0 +1,11 @@
+package fundamentals;
+
+public class VariableTest {
+	//the structure of the declaration of a variable:
+	//type of the variable + name of the variable;
+    String name = "Guille";
+    public static void main(String[] args) {
+       System.out.println(1223123);   
+    }
+
+}
