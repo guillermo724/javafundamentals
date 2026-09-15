4a3792d8d3cc36c54d5f90a873a81e965c3b481c
guillermo724 <gmiguel08@colegiovirgendeeuropa.edu.es>
2026-09-15 10:00:24 +0200

    first commit

--- src/fundamentals/VariableTest.java
+++ src/fundamentals/VariableTest.java
@@ -1,5 +1,9 @@
 package fundamentals;
-
+/**
+ * 
+11 sept 2026
+Guillermo
+ */
 public class VariableTest {
 	
 	public static void main(String[] args) {
@@ -13,7 +17,24 @@ public class VariableTest {
        System.out.println("Hola java");
        name = "Asier";
        System.out.println(name);
+       //Declaration of an integer variable
+       //an integer has 4 bytes
+       //1 byte is 8 bits
+       int age = 23432432;
+       long weight = 234324234324l;//a long variable has 8 bytes
+       float height = 23.3f;// a float variable holds 4 bytes
+       double b = 324.24234234;//a double has 8 bytes
+       byte a = -127;// a byte has only one byte of capacity
        
+       //increase the value of "age" by 10
+       age = age + 10;
+       System.out.println(age);
+       age = age * 10;// * multiplication operator
+       System.out.println(age);
+       weight = weight - 1000000;
+       System.out.println(weight);
+       height = height / 100;// division
+       System.out.println(height);
     }
 
 }
